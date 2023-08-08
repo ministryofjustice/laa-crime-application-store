e01b4250e0c425f2fa8a0490d207a2235934e6c7
Paul King <84575729+pking-moj@users.noreply.github.com>
2023-08-08 10:30:44 +0100

    CRM457-361 - Add Ping Endpoint (#5)
    ## Description of change
    
    Adds a Ping endpoint to easily see information about the deployed
    application
    
    ## Link to relevant ticket
    
    [CRM457-361](https://dsdmoj.atlassian.net/browse/CRM457-361)
    
    ## Notes for reviewer
    
    ## Screenshots of changes (if applicable)
    
    ### Before changes:
    
    ### After changes:
    
    ## How to manually test the feature
    
    [CRM457-361]:
    https://dsdmoj.atlassian.net/browse/CRM457-361?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ laa_crime_application_store_app/config/app_settings.py
@@ -0,0 +1,23 @@
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class AppSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        extra="ignore", env_file=".env", env_file_encoding="utf-8"
+    )
+
+    app_name: str = "Laa Crime Application Store"
+    app_repo: str = "https://github.com/ministryofjustice/laa-crime-application-store"
+    contact_email: str = "crm457@digital.justice.gov.uk"
+    contact_team: str = "CRM457"
+    commit_id: str | None = None
+    build_date: str | None = None
+    build_tag: str | None = None
+    app_branch: str | None = None
+
+
+@lru_cache()
+def get_app_settings():
+    return AppSettings()
--- laa_crime_application_store_app/main.py
+++ laa_crime_application_store_app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from starlette.responses import RedirectResponse
 
-app = FastAPI()
+from laa_crime_application_store_app.config.app_settings import get_app_settings
+from laa_crime_application_store_app.routers import ping
+
+app = FastAPI(
+    title=get_app_settings().app_name,
+    version="0.0.1",
+    contact={
+        "name": get_app_settings().contact_team,
+        "email": get_app_settings().contact_email,
+        "url": get_app_settings().app_repo,
+    },
+)
+
+app.include_router(ping.router)
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def get_index():
+    return RedirectResponse(url="/docs")
