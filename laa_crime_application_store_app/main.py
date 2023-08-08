36cb6352db4f0de5d1f1c77792726dbd48d33910
Paul King <84575729+pking-moj@users.noreply.github.com>
2023-08-08 12:56:21 +0100

    Add structlog to app

--- laa_crime_application_store_app/main.py
+++ laa_crime_application_store_app/main.py
@@ -1,8 +1,31 @@
+import logging.config
+
+import structlog
+from asgi_correlation_id import CorrelationIdMiddleware
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
+from structlog.stdlib import LoggerFactory
 
+from laa_crime_application_store_app.config import logging_config
 from laa_crime_application_store_app.config.app_settings import get_app_settings
 from laa_crime_application_store_app.routers import ping
+
+structlog.configure(
+    logger_factory=LoggerFactory(),
+    processors=[
+        structlog.stdlib.add_logger_name,
+        structlog.stdlib.add_log_level,
+        structlog.stdlib.PositionalArgumentsFormatter(),
+        structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M.%S"),
+        structlog.processors.StackInfoRenderer(),
+        structlog.processors.format_exc_info,
+        structlog.processors.JSONRenderer(),
+    ],
+    wrapper_class=structlog.stdlib.BoundLogger,
+    cache_logger_on_first_use=True,
+)
+logging.config.dictConfig(logging_config.config)
+
 
 app = FastAPI(
     title=get_app_settings().app_name,
@@ -13,6 +36,8 @@ app = FastAPI(
         "url": get_app_settings().app_repo,
     },
 )
+
+app.add_middleware(CorrelationIdMiddleware)
 
 app.include_router(ping.router)
 
