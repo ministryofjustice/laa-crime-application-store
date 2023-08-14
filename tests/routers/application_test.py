68af701ceb11a726112d346e947407b794596ae9
David Henry <david.henry@digital.justice.gov.uk>
2023-08-14 11:15:47 +0100

    Update DB structure

--- tests/routers/application_test.py
+++ tests/routers/application_test.py
@@ -25,6 +25,36 @@ def test_post_application_returns_200(client, db):
     )
 
     assert db.query(Application).count() == 1
+    assert response.status_code == 201
+
+
+def test_returns_duplicate_error_if_id_already_exists(client, db):
+    new_application = Application(
+        id="d7f509e8-309c-4262-a41d-ebbb44deab9e",
+        claim_id="d7f509e8-309c-4262-a41d-ebbb44deab9e",
+        version=1,
+        json_schema_version=1,
+        application_state="submitted",
+        application_risk="high",
+        application={},
+    )
+
+    db.add(new_application)
+    db.commit()
+
+    response = client.post(
+        "/application/",
+        headers={"X-Token": "coneofsilence", "Content-Type": "application/json"},
+        json={
+            "claim_id": "d7f509e8-309c-4262-a41d-ebbb44deab9e",
+            "json_schema_version": 1,
+            "application_state": "submitted",
+            "risk": "high",
+            "application": {"id": 10},
+        },
+    )
+
+    assert db.query(Application).count() == 1
 
     logger.info("response", response=response.headers)
     assert response.status_code == 201
