de44951095b21a69ff6e98313ced5b564587ae2e
Paul King <84575729+pking-moj@users.noreply.github.com>
2023-11-15 15:55:56 +0000

    Move get logic to service (#32)
    ## Description of change
    
    - Move get by id logic into service
    
    ## Link to relevant ticket
    
    N/A
    
    ## Notes for reviewer
    
    ## Screenshots of changes (if applicable)
    
    ### Before changes:
    
    ### After changes:
    
    ## How to manually test the feature

--- laa_crime_application_store_app/services/v1/application_service.py
+++ laa_crime_application_store_app/services/v1/application_service.py
@@ -1,15 +1,59 @@
 from datetime import datetime
+from uuid import UUID
 
+import structlog
 from sqlalchemy.orm import Session
 
 from laa_crime_application_store_app.models.application_schema import Application
+from laa_crime_application_store_app.models.application_version_schema import (
+    ApplicationVersion,
+)
+from laa_crime_application_store_app.schema.application import Application as App
 from laa_crime_application_store_app.schema.basic_application import (
     ApplicationResponse,
     BasicApplication,
 )
 
+logger = structlog.getLogger(__name__)
+
 
 class ApplicationService:
+    @staticmethod
+    def get_application(db: Session, app_id: UUID):
+        application = db.query(Application).filter(Application.id == app_id).first()
+
+        if application is None:
+            logger.info("APPLICATION_NOT_FOUND", application_id=app_id)
+            return None
+
+        application_version = (
+            db.query(ApplicationVersion)
+            .filter(
+                ApplicationVersion.application_id == app_id,
+                ApplicationVersion.version == application.current_version,
+            )
+            .first()
+        )
+
+        if application_version is None:
+            logger.info(
+                "APPLICATION_VERSION_NOT_FOUND",
+                application_id=app_id,
+                version=application.current_version,
+            )
+            return None
+
+        return App(
+            application_id=app_id,
+            version=application_version.version,
+            json_schema_version=application_version.json_schema_version,
+            application_state=application.application_state,
+            application_risk=application.application_risk,
+            events=application.events or [],
+            application_type=application.application_type,
+            application=application_version.application,
+        )
+
     @staticmethod
     def get_applications(db: Session, since: int | None = None, count: int | None = 20):
         applications = (
