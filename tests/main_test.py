eedfe544c1515b8f72b0712bb31dd149a934fabd
Paul King <84575729+pking-moj@users.noreply.github.com>
2023-12-20 16:50:07 +0000

    Fix broken test

--- tests/main_test.py
+++ tests/main_test.py
@@ -5,9 +5,9 @@ from laa_crime_application_store_app.main import app
 client = TestClient(app)
 
 
-def test_main_route_returns_302():
+def test_main_route_returns_404_when_disabled():
     response = client.get("/")
-    assert response.status_code == 200
+    assert response.status_code == 404
 
 
 def test_main_route_body_returns_hello_world():
