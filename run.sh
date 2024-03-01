27d3baef58c773875e0c754241c5b3200d445fa0
David Henry <david.henry@digital.justice.gov.uk>
2024-03-01 12:03:05 +0000

    logging

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 cd /usr/src/app
 
-python ./alembic/generate_db.py && uvicorn laa_crime_application_store_app.main:app --host 0.0.0.0 --port 8000
+echo 'Username'
+echo $POSTGRES_USERNAME
+echo 'pass'
+echo $POSTGRES_PASSWORD
+echo 'host'
+echo $POSTGRES_HOSTNAME
+echo 'name'
+echo $POSTGRES_NAME
+
+python ./alembic/generate_db.py && uvicorn laa_crime_application_store_app.main:app --host 0.0.0.0 --port 8000echo 'Username'
