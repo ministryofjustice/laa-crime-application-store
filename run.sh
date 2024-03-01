e34b2d195225ea715733ade950fa21cdfe0c3032
Jo Gilder <3441835+jogilder@users.noreply.github.com>
2024-03-01 13:01:26 +0000

    remove generat_db command

--- run.sh
+++ run.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 cd /usr/src/app
 
-echo 'Username'
-echo $POSTGRES_USERNAME
-echo 'pass'
-echo $POSTGRES_PASSWORD
-echo 'host'
-echo $POSTGRES_HOSTNAME
-echo 'name'
-echo $POSTGRES_NAME
+python ./alembic/generate_db.py && uvicorn laa_crime_application_store_app.main:app --host 0.0.0.0 --port 8000
 
-python ./alembic/generate_db.py && uvicorn laa_crime_application_store_app.main:app --host 0.0.0.0 --port 8000echo 'Username'
