ab4140c793fdb061187b9beaa36b6c63b24a1a71
David Henry <david.henry@digital.justice.gov.uk>
2024-03-22 10:58:39 +0000

    More complete states list

--- laa_crime_application_store_app/services/permissions.py
+++ laa_crime_application_store_app/services/permissions.py
@@ -1,11 +1,14 @@
 from typing import List
 
+from sentry_sdk import capture_message
+
 from laa_crime_application_store_app.models.application_schema import Application
 
 
 class Permissions:
-    LOCKED_STATES = "granted"
-    CASEWORKER_EDITABLE_STATES = "submitted"
+    LOCKED_STATES = ("granted", "expired", "auto_grant")
+    CASEWORKER_EDITABLE_STATES = ("submitted", "provider_updated")
+    PROVIDER_EDITABLE_STATES = ("part_granted", "rejected", "part_grant", "sent_back")
 
     CASEWORKER_ROLE = "caseworker"
     PROVIDER_ROLE = "provider"
@@ -21,10 +24,14 @@ class Permissions:
         elif application.application_state in Permissions.CASEWORKER_EDITABLE_STATES:
             if Permissions.CASEWORKER_ROLE not in roles:
                 return False
-        else:
+        elif application.application_state in Permissions.PROVIDER_EDITABLE_STATES:
             if Permissions.PROVIDER_ROLE not in roles:
                 return False
 
+        # fallback for unknown state
+        capture_message(
+            f"Unknown state {application.application_state} when check permission logic"
+        )
         return True
 
 
