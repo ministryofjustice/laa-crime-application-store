38843bacc2db79046c0cdae7ee7b163615e74a08
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2024-04-16 14:27:09 +0100

    Fix deployment issues (#122)
    ## Description of change
    - Postman `ping` test expects ping endpoint to return some specific
    stuff in payload
    - UAT and prod environments were configured with wrong security settings
    - build-and-deploy-open-pr workflow was running on main

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -2,6 +2,17 @@ class HealthController < ApplicationController
   skip_before_action :authenticate!, only: :show
 
   def show
-    render json: { healthy: :yes }, status: :ok
+    render json: build_args, status: :ok
+  end
+
+private
+
+  def build_args
+    {
+      branch_name: ENV.fetch("APP_BRANCH_NAME", nil),
+      build_date: ENV.fetch("APP_BUILD_DATE", nil),
+      build_tag: ENV.fetch("APP_BUILD_TAG", nil),
+      commit_id: ENV.fetch("APP_GIT_COMMIT", nil),
+    }
   end
 end
