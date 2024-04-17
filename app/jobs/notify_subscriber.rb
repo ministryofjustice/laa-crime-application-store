6755d047ecfef2bf43df2df8a0a4f530c0d8a06e
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2024-04-17 10:22:43 +0100

    CRM457-1327: More fixes (#123)
    ## Description of change
    - Another fix to ping endpoint to match postman expectations
    - Handle Socket::ResolutionError the same way as other errors in
    NotifySubscriber
    - Bump UAT and production pod resources to be able to handle a `rails c`
    alongside ` rails s`
    - Allow puma to launch in cluster mode so we can handle a synchronous
    response to our webhook callbacks in E2E tests
    - Use fallback request header to identify client role in non-auth setup.

--- app/jobs/notify_subscriber.rb
+++ app/jobs/notify_subscriber.rb
@@ -5,15 +5,7 @@ class NotifySubscriber < ApplicationJob
   def perform(subscriber_id, submission_id)
     subscriber = Subscriber.find(subscriber_id)
 
-    response = HTTParty.post(
-      subscriber.webhook_url,
-      headers:,
-      body: {
-        submission_id:,
-      }.to_json,
-    )
-
-    return if response.code == 200
+    return if send_message_to_webhook(subscriber.webhook_url, submission_id)
 
     subscriber.with_lock do
       subscriber.failed_attempts += 1
@@ -22,7 +14,22 @@ class NotifySubscriber < ApplicationJob
 
     return if delete_subscriber(subscriber)
 
-    raise ClientResponseError, "Failed to notify subscriber about #{submission_id} - #{subscriber.webhook_url} returned #{response.code}"
+    raise ClientResponseError, "Failed to notify subscriber about #{submission_id} - #{subscriber.webhook_url} returned error"
+  end
+
+  def send_message_to_webhook(webhook_url, submission_id)
+    response = HTTParty.post(
+      webhook_url,
+      headers:,
+      body: {
+        submission_id:,
+      }.to_json,
+    )
+
+    response.code == 200
+  rescue Socket::ResolutionError
+    # If the subscriber web app has been taken fully offline and its DNS removed, this will fire
+    false
   end
 
   def delete_subscriber(subscriber)
