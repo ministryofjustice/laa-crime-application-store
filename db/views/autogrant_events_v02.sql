052caf375e5001b7987ab8572dca352dde233006
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-07-08 18:26:53 +0100

    update autogrant events view
    user left join to prevent missing columns where no translation present
    simplify service field logic

--- db/views/autogrant_events_v02.sql
+++ db/views/autogrant_events_v02.sql
@@ -1,16 +1,14 @@
 SELECT e.id,
        e.submission_version,
        e.event_on,
-       ( a.application ->> 'service_type' ) AS service_key,
-        CASE
-          WHEN ( a.application ->> 'service_type' ) = 'custom' THEN a.application ->> 'custom_service_name'
-          ELSE s.translation
-        END AS service
-FROM   all_events e
-       INNER JOIN application_version a
-               ON a.application_id = e.id
-                  AND a.version = e.submission_version
-       INNER JOIN service_translations s
-               ON ( a.application ->> 'service_type' ) = s.key
-WHERE  e.application_type = 'crm4'
-       AND e.event_type = 'auto_decision'
+       ( a.application ->> 'service_type' :: text ) AS service_key,
+       Coalesce(a.application ->> 'custom_service_name',
+        Coalesce(s.translation, a.application ->> 'service_type' :: text)
+        ) AS service
+FROM   ((all_events e
+         join application_version a
+           ON (( ( a.application_id = e.id )
+                 AND ( a.version = e.submission_version ) )))
+        left join service_translations s
+               ON (( ( a.application ->> 'service_type' :: text ) =
+                  ( s.KEY ) :: text )))
