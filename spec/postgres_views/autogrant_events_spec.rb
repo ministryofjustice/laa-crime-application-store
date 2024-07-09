204734c0e0c6395e96326215ed88bb84fe5de2d2
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-07-09 15:28:22 +0100

    add additional tests to remove for testing

--- spec/postgres_views/autogrant_events_spec.rb
+++ spec/postgres_views/autogrant_events_spec.rb
@@ -18,6 +18,8 @@ RSpec.describe "autogrant_events" do
 
     expect(klass.all).to eq([])
     expect(Translation.all).to eq([])
+    expect(Submission.all).to eq([])
+    expect(SubmissionVersion.all).to eq([])
   end
 
   it "when autogranted application exists with a translated service" do
