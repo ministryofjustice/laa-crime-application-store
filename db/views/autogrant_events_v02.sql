5eb652223c8026fe4f40fc3c49c521263632b38a
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-07-09 15:55:35 +0100

    fix translation join

--- db/views/autogrant_events_v02.sql
+++ db/views/autogrant_events_v02.sql
@@ -8,9 +8,9 @@ FROM   ((all_events e
          join application_version a
            ON (( ( a.application_id = e.id )
                  AND ( a.version = e.submission_version ) )))
-        left join translations s
+         left join translations s
                ON (( ( a.application ->> 'service_type' :: text ) =
-                  ( s.KEY ) :: text )))
+                  ( s.key  :: text ))))
+               AND (s.translation_type :: text)  = 'service'
 WHERE  e.application_type = 'crm4'
        AND e.event_type = 'auto_decision'
-       AND s.translation_type = 'service'
