ddfc1aa6b990dbba5b3070d9df51b231843e4703
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-07-09 15:59:25 +0100

    Update autogrant_events_v02.sql

--- db/views/autogrant_events_v02.sql
+++ db/views/autogrant_events_v02.sql
@@ -1,4 +1,5 @@
 SELECT e.id,
+       e.submission_version,
        e.event_on,
        ( a.application ->> 'service_type' :: text ) AS service_key,
        Coalesce(a.application ->> 'custom_service_name',
@@ -8,9 +9,9 @@ FROM   ((all_events e
          join application_version a
            ON (( ( a.application_id = e.id )
                  AND ( a.version = e.submission_version ) )))
-         left join translations s
+        left join service_translations s
                ON (( ( a.application ->> 'service_type' :: text ) =
-                  ( s.key  :: text ))))
-               AND (s.translation_type :: text)  = 'service'
+                  ( s.key ) :: text )))
+               AND s.translation_type = 'service'
 WHERE  e.application_type = 'crm4'
        AND e.event_type = 'auto_decision'
