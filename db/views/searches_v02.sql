f9858372fd904f716404f780dd5f5ca433c142bf
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2024-07-09 14:17:24 +0100

    Update searches view v1 to v2
    Add sortable fields

---
+++ db/views/searches_v02.sql
@@ -0,0 +1,15 @@
+SELECT
+  app.id,
+  app_ver.id as application_version_id,
+  app_ver.application ->> 'laa_reference' as laa_reference,
+  app_ver.application -> 'firm_office' ->> 'name' as firm_name,
+  (app_ver.application -> 'defendant' ->> 'first_name') || ' ' || (app_ver.application -> 'defendant' ->> 'last_name') as client,
+  app_ver.search_fields,
+  app.has_been_assigned_to,
+  app.created_at as date_submitted,
+  app.updated_at as date_updated, -- events can happen in caseworker after this? should we be checking max event time as well???
+  app.application_state as status,
+  app.application_type as submission_type,
+  app.application_risk as risk
+FROM application AS app
+JOIN application_version AS app_ver on app.id = app_ver.application_id and app.current_version = app_ver.version
