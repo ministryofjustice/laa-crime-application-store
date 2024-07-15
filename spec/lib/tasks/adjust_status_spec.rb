c28ca0766e91b511ca808d32e752ca4bd9a48d5a
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2024-07-15 09:09:30 +0100

    CRM457-1747: Task to adjust status (#217)
    ## Description of change
    We need to fix some data in the provider app, and rather than manually
    fix it in every other database we can push through an updated payload.
    BUT to do so we need to temporarily put affected submissions into a
    state that permits provider updates. This task lets us do this. Intended
    usage:
    
    - Identify affected IDs in provider
    - Use this to set status of those to `sent_back` WITHOUT sending
    notifications
    - Run the fix in provider
    - Use this to set the status of them back to their initial state,
    sending notifications to caseworker to make sure it ends up tracking the
    correct state.
    
    [Link to relevant
    ticket](https://dsdmoj.atlassian.net/browse/CRM457-1747)

---
+++ spec/lib/tasks/adjust_status_spec.rb
@@ -0,0 +1,43 @@
+require "rails_helper"
+
+describe "adjust:status", type: :task do
+  let(:notifier) { instance_double(NotifySubscriber, perform: true) }
+  let(:submission) { create(:submission, application_state: "granted") }
+  let(:subscriber) { create(:subscriber, subscriber_type: "caseworker") }
+  let(:args) { [submission.id, "rejected"] }
+
+  before do
+    Rails.application.load_tasks if Rake::Task.tasks.empty?
+    allow(NotifySubscriber).to receive(:new).and_return(notifier)
+    subscriber
+    Rake::Task["adjust:status"].invoke(*args)
+  end
+
+  after do
+    Rake::Task["adjust:status"].reenable
+  end
+
+  it "updates the status" do
+    expect(submission.reload.application_state).to eq "rejected"
+  end
+
+  it "does not notify anyone by default" do
+    expect(notifier).not_to have_received(:perform)
+  end
+
+  context "when a different role to notify is specified" do
+    let(:args) { [submission.id, "rejected", "provider"] }
+
+    it "does not notify the subscriber" do
+      expect(notifier).not_to have_received(:perform)
+    end
+  end
+
+  context "when the same role to notify is specified" do
+    let(:args) { [submission.id, "rejected", "caseworker"] }
+
+    it "notifies the subscriber" do
+      expect(notifier).to have_received(:perform).with(subscriber.id, submission.id)
+    end
+  end
+end
