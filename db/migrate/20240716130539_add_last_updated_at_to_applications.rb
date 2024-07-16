bbb089bae24d8603f09e6f488f73b5b1d40b1a37
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2024-07-16 16:31:00 +0100

    fix other migration due to column existing

--- db/migrate/20240716130539_add_last_updated_at_to_applications.rb
+++ db/migrate/20240716130539_add_last_updated_at_to_applications.rb
@@ -1,5 +1,5 @@
 class AddLastUpdatedAtToApplications < ActiveRecord::Migration[7.1]
   def change
-    add_column :application, :last_updated_at, :datetime, precision: nil
+    add_column :application, :last_updated_at, :datetime, precision: nil, if_not_exists: true
   end
 end
