ed52d85db34a72c11b380e2ff61dfe459fa75670
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2024-07-16 16:04:06 +0100

    fix undefined method error in migration

--- db/migrate/20240716130618_backfill_last_updated_at.rb
+++ db/migrate/20240716130618_backfill_last_updated_at.rb
@@ -6,7 +6,7 @@ class BackfillLastUpdatedAt < ActiveRecord::Migration[7.1]
       # Update all last_updated_at columns to be the latest event date
       # or, if no events exist, then the latest application version created_at date.
       #
-      latest_event = submission.events&.max_by { |event| event['created_at'].to_time }
+      latest_event = submission.events&.max_by { |event| event['created_at']&.to_time }
 
       last_updated_at  = if latest_event
         last_updated_at = latest_event['created_at'].to_time
