e00a960e8a2dac8363b1cad4aee1cd518f5ebac9
David Henry <david.henry@digital.justice.gov.uk>
2024-07-26 12:07:18 +0100

    linting

--- spec/postgres_views/active_providers_spec.rb
+++ spec/postgres_views/active_providers_spec.rb
@@ -15,21 +15,21 @@ RSpec.describe "Active Providers" do
     travel_to(base_date - 7) { create(:submission, :with_pa_version, account_number: "B345FF") }
 
     expect(klass.all.map(&:attributes)).to eq([
-      {"application_type"=>"crm4",
-        "office_codes_during_the_period"=>["1A123B"],
-        "office_codes_submitting_during_the_period"=>1,
-        "submitted_start"=>Date.new(2024, 06, 10),
-        "total_office_codes_submitters"=>1},
-      {"application_type"=>"crm4",
-        "office_codes_during_the_period"=>["1A123B", "B345FF"],
-        "office_codes_submitting_during_the_period"=>2,
-        "submitted_start"=>Date.new(2024, 06, 17),
-        "total_office_codes_submitters"=>2},
-      {"application_type"=>"crm4",
-        "office_codes_during_the_period"=>["B345FF"],
-        "office_codes_submitting_during_the_period"=>1,
-        "submitted_start"=>Date.new(2024, 06, 24),
-        "total_office_codes_submitters"=>2}
+      { "application_type" => "crm4",
+        "office_codes_during_the_period" => %w[1A123B],
+        "office_codes_submitting_during_the_period" => 1,
+        "submitted_start" => Date.new(2024, 0o6, 10),
+        "total_office_codes_submitters" => 1 },
+      { "application_type" => "crm4",
+        "office_codes_during_the_period" => %w[1A123B B345FF],
+        "office_codes_submitting_during_the_period" => 2,
+        "submitted_start" => Date.new(2024, 0o6, 17),
+        "total_office_codes_submitters" => 2 },
+      { "application_type" => "crm4",
+        "office_codes_during_the_period" => %w[B345FF],
+        "office_codes_submitting_during_the_period" => 1,
+        "submitted_start" => Date.new(2024, 0o6, 24),
+        "total_office_codes_submitters" => 2 },
     ])
   end
 
@@ -41,26 +41,26 @@ RSpec.describe "Active Providers" do
     travel_to(base_date - 7) { create(:submission, :with_nsm_version, account_number: "B345FF") }
 
     expect(klass.all.map(&:attributes)).to eq([
-      {"application_type"=>"crm4",
-        "office_codes_during_the_period"=>["1A123B"],
-        "office_codes_submitting_during_the_period"=>1,
-        "submitted_start"=>Date.new(2024, 06, 10),
-        "total_office_codes_submitters"=>1},
-      {"application_type"=>"crm4",
-        "office_codes_during_the_period"=>["1A123B"],
-        "office_codes_submitting_during_the_period"=>1,
-        "submitted_start"=>Date.new(2024, 06, 24),
-        "total_office_codes_submitters"=>1},
-      {"application_type"=>"crm7",
-        "office_codes_during_the_period"=>["1A123B", "B345FF"],
-        "office_codes_submitting_during_the_period"=>2,
-        "submitted_start"=>Date.new(2024, 06, 17),
-        "total_office_codes_submitters"=>2},
-      {"application_type"=>"crm7",
-        "office_codes_during_the_period"=>["B345FF"],
-        "office_codes_submitting_during_the_period"=>1,
-        "submitted_start"=>Date.new(2024, 06, 24),
-        "total_office_codes_submitters"=>2}
+      { "application_type" => "crm4",
+        "office_codes_during_the_period" => %w[1A123B],
+        "office_codes_submitting_during_the_period" => 1,
+        "submitted_start" => Date.new(2024, 0o6, 10),
+        "total_office_codes_submitters" => 1 },
+      { "application_type" => "crm4",
+        "office_codes_during_the_period" => %w[1A123B],
+        "office_codes_submitting_during_the_period" => 1,
+        "submitted_start" => Date.new(2024, 0o6, 24),
+        "total_office_codes_submitters" => 1 },
+      { "application_type" => "crm7",
+        "office_codes_during_the_period" => %w[1A123B B345FF],
+        "office_codes_submitting_during_the_period" => 2,
+        "submitted_start" => Date.new(2024, 0o6, 17),
+        "total_office_codes_submitters" => 2 },
+      { "application_type" => "crm7",
+        "office_codes_during_the_period" => %w[B345FF],
+        "office_codes_submitting_during_the_period" => 1,
+        "submitted_start" => Date.new(2024, 0o6, 24),
+        "total_office_codes_submitters" => 2 },
     ])
   end
 end
