c78459f1f9aff13b1fc01837fcc41c3b7b04a5ad
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-08-06 17:16:35 +0100

    add tests and fix issues with printing

--- lib/tasks/check_autogrants.rake
+++ lib/tasks/check_autogrants.rake
@@ -1,13 +1,13 @@
 
 namespace :check_autogrants do
   desc "Check submissions with autogranted versions but no auto decision event"
-  task :retrieve_all => :environment do |_, args|
+  task :retrieve_all => :environment do
     submissions_missing_event = []
-    autogranted_submissions = Submission.where(application_state: 'auto_grant')
+    autogranted_submissions = Submission.where(application_state: 'auto_grant').order(updated_at: :asc)
     autogranted_submissions.each do |submission|
       autogrant_event = submission.events.filter { _1['event_type'] == 'auto_decision' }
-      submissions_missing_event << submission.id
+      submissions_missing_event << submission.id unless autogrant_event.present?
     end
-    puts submissions_missing_event
+    print submissions_missing_event.join(",")
   end
 end
---
+++ spec/lib/tasks/check_autogrants_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe "check_autogrants:retrieve_all", type: :task do
+  let(:valid_submissions) { create_list(:submission, 2, :with_pa_version, events: [build(:event, :new_version), build(:event, :auto_decision)], application_state: 'auto_grant') }
+  let(:faulty_submissions) { create_list(:submission, 2, :with_pa_version, events: [build(:event, :new_version)], application_state: 'auto_grant') }
+
+  before do
+    valid_submissions
+    faulty_submissions
+    Rails.application.load_tasks if Rake::Task.tasks.empty?
+  end
+
+  after do
+    Rake::Task["check_autogrants:retrieve_all"].reenable
+  end
+
+  it 'prints out the correct submission ids' do
+    expected_output = faulty_submissions.sort_by{ _1.updated_at }.map{ _1.id }.join(",")
+    expect { Rake::Task["check_autogrants:retrieve_all"].execute }.to output(expected_output).to_stdout
+  end
+end
