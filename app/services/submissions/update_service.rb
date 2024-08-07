af4d004aece728a6d7d51dc81a6dc2fbe87cbe96
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-08-07 15:46:19 +0100

    Add lock to add_events method

--- app/services/submissions/update_service.rb
+++ app/services/submissions/update_service.rb
@@ -12,20 +12,22 @@ module Submissions
       end
 
       def add_events(submission, params, save: false)
-        submission.events ||= []
-        params[:events]&.each do |event|
-          next if submission.events.any? { _1["id"] == event["id"] }
+        submission.with_lock do
+          submission.events ||= []
+          params[:events]&.each do |event|
+            next if submission.events.any? { _1["id"] == event["id"] }
 
-          event["submission_version"] ||= submission.current_version
-          event["created_at"] ||= Time.zone.now
-          event["updated_at"] ||= event["created_at"]
+            event["submission_version"] ||= submission.current_version
+            event["created_at"] ||= Time.zone.now
+            event["updated_at"] ||= event["created_at"]
 
-          submission.events << event.as_json
+            submission.events << event.as_json
+          end
+
+          latest_event = submission.events.max_by { |ev| ev["created_at"] }
+          submission.last_updated_at = latest_event["created_at"] if latest_event
+          save && submission.save!
         end
-
-        latest_event = submission.events.max_by { |ev| ev["created_at"] }
-        submission.last_updated_at = latest_event["created_at"] if latest_event
-        save && submission.save!
       end
 
       def add_new_version(submission, params)
