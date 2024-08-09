b1d63d79bd79cbdcff9009b86d8e7181aa3be200
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-08-09 14:46:15 +0100

    Address review comment

--- db/migrate/20240809094250_back_fill_assessment_comments.rb
+++ db/migrate/20240809094250_back_fill_assessment_comments.rb
@@ -1,24 +1,23 @@
-class BackFillAssessmentComments < ActiveRecord::Migration[7.1]
-  class BackfillAssessmentComments < ActiveRecord::Migration[7.1]
-    def up
-      Submission.where(application_state: %w[granted rejected part_grant]).find_each do |submission|
-        event = submission.events.detect { _1['event_type'] == 'decision' }
-        next unless event
+class BackfillAssessmentComments < ActiveRecord::Migration[7.1]
+  disable_ddl_transaction!
 
-        version = submission.latest_version
-        version.application['assessment_comment'] = event.dig('details', 'comment')
-        version.save!(touch: false)
-      end
+  def up
+    Submission.where(application_state: %w[granted rejected part_grant]).find_each do |submission|
+      event = submission.events.detect { _1['event_type'] == 'decision' }
+      next unless event
 
-      Submission.where(application_state: 'sent_back', application_type: 'crm7').find_each do |submission|
-        event = submission.events.detect { _1['event_type'] == 'send_back' }
-        next unless event
+      version = submission.latest_version
+      version.application['assessment_comment'] = event.dig('details', 'comment')
+      version.save!(touch: false)
+    end
 
-        version = submission.latest_version
-        version.application['assessment_comment'] = event.dig('details', 'comment')
-        version.save!(touch: false)
-      end
+    Submission.where(application_state: 'sent_back', application_type: 'crm7').find_each do |submission|
+      event = submission.events.detect { _1['event_type'] == 'send_back' }
+      next unless event
+
+      version = submission.latest_version
+      version.application['assessment_comment'] = event.dig('details', 'comment')
+      version.save!(touch: false)
     end
   end
-
 end
