cf06d43e06cf9789af1a324e845982f2ff61d8b7
David Henry <david.henry@digital.justice.gov.uk>
2024-08-20 17:34:55 +0100

    linting

--- spec/lib/tasks/adjust_fix_provider_updated_data.rb
+++ spec/lib/tasks/adjust_fix_provider_updated_data.rb
@@ -6,7 +6,7 @@ data = [
   ["14994bfc-95c2-4ddb-94a0-1b84cbc1f7f0", "2024-06-25T12:51:32.494Z", "00061702-ce7b-4dc8-bc6c-ec13cf0ceae2", 2, [[1, "new_version", "2024-06-24T16:15:47.492Z"], [1, "assignment", "2024-06-25T12:48:42.862Z"], [1, "send_back", "2024-06-25T12:51:32.494Z"], [3, "provider_updated", "2024-06-25T15:10:08.417+00:00"], [3, "assignment", "2024-06-26T12:29:08.640Z"], [3, "decision", "2024-06-26T12:33:38.249Z"]], "sent_back"],
   ["73c46945-0a6c-4b3a-a828-661de16edd79", "2024-06-24T16:15:46.909Z", "00061702-ce7b-4dc8-bc6c-ec13cf0ceae2", 3, [[1, "new_version", "2024-06-24T16:15:47.492Z"], [1, "assignment", "2024-06-25T12:48:42.862Z"], [1, "send_back", "2024-06-25T12:51:32.494Z"], [3, "provider_updated", "2024-06-25T15:10:08.417+00:00"], [3, "assignment", "2024-06-26T12:29:08.640Z"], [3, "decision", "2024-06-26T12:33:38.249Z"]], "provider_updated"],
   ["bd080793-01d5-4882-9b42-360f123ec8b7", "2024-06-24T16:15:46.909Z", "00061702-ce7b-4dc8-bc6c-ec13cf0ceae2", 1, [[1, "new_version", "2024-06-24T16:15:47.492Z"], [1, "assignment", "2024-06-25T12:48:42.862Z"], [1, "send_back", "2024-06-25T12:51:32.494Z"], [3, "provider_updated", "2024-06-25T15:10:08.417+00:00"], [3, "assignment", "2024-06-26T12:29:08.640Z"], [3, "decision", "2024-06-26T12:33:38.249Z"]], "submitted"],
-  ["fcc2f94e-a2bd-422e-b59a-53210fa52bd5", "2024-06-26T12:33:38.249Z", "00061702-ce7b-4dc8-bc6c-ec13cf0ceae2", 4, [[1, "new_version", "2024-06-24T16:15:47.492Z"], [1, "assignment", "2024-06-25T12:48:42.862Z"], [1, "send_back", "2024-06-25T12:51:32.494Z"], [3, "provider_updated", "2024-06-25T15:10:08.417+00:00"], [3, "assignment", "2024-06-26T12:29:08.640Z"], [3, "decision", "2024-06-26T12:33:38.249Z"]], "granted"]
+  ["fcc2f94e-a2bd-422e-b59a-53210fa52bd5", "2024-06-26T12:33:38.249Z", "00061702-ce7b-4dc8-bc6c-ec13cf0ceae2", 4, [[1, "new_version", "2024-06-24T16:15:47.492Z"], [1, "assignment", "2024-06-25T12:48:42.862Z"], [1, "send_back", "2024-06-25T12:51:32.494Z"], [3, "provider_updated", "2024-06-25T15:10:08.417+00:00"], [3, "assignment", "2024-06-26T12:29:08.640Z"], [3, "decision", "2024-06-26T12:33:38.249Z"]], "granted"],
 ]
 
 applications = {}
@@ -28,6 +28,6 @@ data.each do |ver_id, ver_updated_at, app_id, ver_version, events_array, ver_sta
   app_hash = FactoryBot.build(:application,
                               status: ver_status,
                               updated_at: ver_updated_at,
-                              created_at: Time.new(2024, 1, 1))
+                              created_at: Time.zone.local(2024, 1, 1))
   FactoryBot.create(:submission_version, submission: application, application: app_hash, version: ver_version, id: ver_id)
 end
--- spec/lib/tasks/adjust_fix_provider_updated_spec.rb
+++ spec/lib/tasks/adjust_fix_provider_updated_spec.rb
@@ -16,9 +16,9 @@ RSpec.describe "adjust:fix_expired" do
     allow(ENV).to receive(:[]).with("PERSIST_ADJUSTMENT").and_return("true")
     Rails.application.load_tasks if Rake::Task.tasks.empty?
 
-    records = klass.where(Arel.sql('from_time > to_time'))
+    klass.where(Arel.sql("from_time > to_time"))
 
-    expect(klass.where(Arel.sql('from_time > to_time')).pluck(:id, :from_time, :to_time)).to eq(
+    expect(klass.where(Arel.sql("from_time > to_time")).pluck(:id, :from_time, :to_time)).to eq(
       [
         ["00061702-ce7b-4dc8-bc6c-ec13cf0ceae2", Time.parse("2024-06-25T12:51:32.494Z"), Time.parse("2024-06-24T16:15:46.909Z")],
       ],
@@ -27,6 +27,6 @@ RSpec.describe "adjust:fix_expired" do
     Rake::Task["adjust:fix_provider_updated"].invoke
 
     # NOTE: 122976 seconds = 14 days
-    expect(klass.where(Arel.sql('from_time > to_time')).pluck(:id)).to be_empty
+    expect(klass.where(Arel.sql("from_time > to_time")).pluck(:id)).to be_empty
   end
 end
