f3f07e6a9dc6e2063d6530a07ae86b56531fc410
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-09-04 18:54:42 +0100

    use correct variable name

--- spec/lib/tasks/find_mismatched_references_spec.rb
+++ spec/lib/tasks/find_mismatched_references_spec.rb
@@ -24,7 +24,7 @@ describe "fixes:find_mismatched_references", type: :task do
 
   it "prints out the correct information" do
     invalid_versions = invalid_submission.ordered_submission_versions.pluck(Arel.sql("application -> 'laa_reference'")).uniq.join(",")
-    expected_output = "Submission ID: #{invalid_submission.id} Original Ref: #{invalid} All References: #{invalid_versions}\n"
+    expected_output = "Submission ID: #{invalid_submission.id} Original Ref: #{invalid_reference} All References: #{invalid_versions}\n"
     expect { Rake::Task["fixes:find_mismatched_references"].execute }.to output(expected_output).to_stdout
   end
 end
