8474158d2bd48e99be6e8d9df25b921ae5e337a2
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-09-04 17:42:08 +0100

    fix creation of test entries

--- spec/lib/tasks/find_mismatched_references_spec.rb
+++ spec/lib/tasks/find_mismatched_references_spec.rb
@@ -2,15 +2,16 @@ require "rails_helper"
 
 describe "fixes:find_mismatched_references", type: :task do
   let(:valid_submission) { create(:submission, :with_pa_version, current_version: 3) }
-  let(:additional_version) { create(:submission_version, :with_pa_application, submission: valid_submission) }
   let(:invalid_submission) { create(:submission, :with_pa_version, current_version: 3) }
-  let(:additional_invalid_version) { create(:submission_version, laa_reference: "LAA-654321", submission: invalid_submission) }
+  let(:additional_invalid_version) { create(:submission_version, laa_reference: "LAA-654321", version: 2, submission: invalid_submission) }
 
   before do
     valid_submission
-    additional_version
+    create(:submission_version, :with_pa_application, version: 2, submission: valid_submission)
+    create(:submission_version, :with_pa_application, version: 3, submission: valid_submission)
     invalid_submission
-    additional_invalid_version
+    create(:submission_version, laa_reference: "LAA-654321", version: 2, submission: invalid_submission)
+    create(:submission_version, laa_reference: "LAA-654321", version: 2, submission: invalid_submission)
     Rails.application.load_tasks if Rake::Task.tasks.empty?
   end
 
