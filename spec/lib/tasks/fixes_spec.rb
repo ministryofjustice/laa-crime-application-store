9ecfd42f8062b1a8aa8123ad556c01ff206967bc
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-09-11 17:57:16 +0100

    add additional tests

--- spec/lib/tasks/fixes_spec.rb
+++ spec/lib/tasks/fixes_spec.rb
@@ -52,7 +52,7 @@ describe "fixes:", type: :task do
 
   describe "fix_corrupt_versions" do
     let(:affected_submission) { create(:submission, :with_pa_version, id: "dec31825-1bd1-461e-8857-5ddf9f839992", current_version: 3) }
-    let(:unaffected_submission) { create(:submission, :with_pa_version, current_version: 3)}
+    let(:unaffected_submission) { create(:submission, :with_pa_version, current_version: 3) }
 
     before do
       affected_submission
@@ -68,10 +68,26 @@ describe "fixes:", type: :task do
       Rake::Task["fixes:fix_corrupt_versions"].reenable
     end
 
-    it 'removes correct submission version' do
+    it "removes correct submission version" do
       Rake::Task["fixes:fix_corrupt_versions"].execute
-      expect { SubmissionVersion.find_by(application_id: affected_submission.id, version: 2) }.to eq(nil)
-      expect { SubmissionVersion.find_by(application_id: unaffected_submission.id, version: 2)}.to be_a SubmissionVersion
+      expect { SubmissionVersion.find_by(application_id: affected_submission.id, version: 2) }.to be_nil
+      expect { SubmissionVersion.find_by(application_id: unaffected_submission.id, version: 2) }.to be_a SubmissionVersion
+    end
+
+    it "decrements the correct submission versions" do
+      Rake::Task["fixes:fix_corrupt_versions"].execute
+      expect { SubmissionVersion.find_by(application_id: affected_submission.id, version: 1) }.to be_a SubmissionVersion
+      expect { SubmissionVersion.find_by(application_id: affected_submission.id, version: 2) }.to be_a SubmissionVersion
+      expect { SubmissionVersion.find_by(application_id: affected_submission.id, version: 3) }.to be_nil
+
+      expect { SubmissionVersion.find_by(application_id: unaffected_submission.id, version: 1) }.to be_a SubmissionVersion
+      expect { SubmissionVersion.find_by(application_id: unaffected_submission.id, version: 2) }.to be_a SubmissionVersion
+      expect { SubmissionVersion.find_by(application_id: unaffected_submission.id, version: 3) }.to be_a SubmissionVersion
+    end
+
+    it "resets submission current_version" do
+      expect { Submission.find(affected_submission.id).current_version }.to eq(2)
+      expect { Submission.find(unaffected_submission.id).current_version }.to eq(3)
     end
   end
 end
