5280126bfcd3fcad76282b8b10e55fbe8a4b4426
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-10-30 13:23:44 +0000

    Update definition of submission_by_date view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_23_170454) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_24_095320) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
