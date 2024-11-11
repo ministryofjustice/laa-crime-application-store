c6edfb0db5ead98e1e65b31fe4ddc9e2d6f92ce2
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-11-11 10:20:40 +0000

    Allow ignoring event timestamp

--- spec/requests/create_events_spec.rb
+++ spec/requests/create_events_spec.rb
@@ -2,8 +2,9 @@ require "rails_helper"
 
 RSpec.describe "Create events" do
   context "when authenticated with bearer token" do
-    let(:submission) { create(:submission, state:) }
+    let(:submission) { create(:submission, state:, last_updated_at: old_timestamp) }
     let(:state) { "submitted" }
+    let(:old_timestamp) { 3.days.ago }
 
     before { allow(Tokens::VerificationService).to receive(:call).and_return(valid: true, role: :caseworker) }
 
@@ -73,6 +74,57 @@ RSpec.describe "Create events" do
       }
       expect(response).to have_http_status :created
       expect(submission.reload.state).to eq("submitted")
+    end
+
+    it "bumps last updated at to implicit timestamp" do
+      freeze_time
+
+      post "/v1/submissions/#{submission.id}/events", params: {
+        events: [
+          {
+            id: "A",
+            details: "history",
+          },
+        ],
+      }
+      expect(response).to have_http_status :created
+
+      expect(submission.reload.last_updated_at).to eq Time.current
+    end
+
+    it "bumps last updated at to explicit timestamp" do
+      freeze_time
+
+      post "/v1/submissions/#{submission.id}/events", params: {
+        events: [
+          {
+            id: "A",
+            details: "history",
+            created_at: 2.hours.ago,
+          },
+        ],
+      }
+      expect(response).to have_http_status :created
+
+      expect(submission.reload.last_updated_at).to eq 2.hours.ago
+    end
+
+    it "ignores timestamps if commandeed" do
+      freeze_time
+
+      post "/v1/submissions/#{submission.id}/events", params: {
+        events: [
+          {
+            id: "A",
+            details: "history",
+            created_at: 2.hours.ago,
+            does_not_constitute_update: true,
+          },
+        ],
+      }
+      expect(response).to have_http_status :created
+
+      expect(submission.reload.last_updated_at).to eq 3.days.ago
     end
   end
 end
