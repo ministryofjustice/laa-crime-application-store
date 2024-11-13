8ce09324e9f234b90949d0f4d1e4b3e1a49fe5e6
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-11-13 15:57:44 +0000

    Fix typo

--- app/mailers/email_to_provider_mailer.rb
+++ app/mailers/email_to_provider_mailer.rb
@@ -2,7 +2,7 @@ class EmailToProviderMailer < GovukNotifyRails::Mailer
   # :nocov:
   def notify(message_class, submission)
     submission.with_lock do
-      message = message_class.new(submission.latest_version.data)
+      message = message_class.new(submission.latest_version.application)
       set_template(message.template)
       set_personalisation(**message.contents)
       mail(to: message.recipient)
