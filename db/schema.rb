f42fecad4f8a65ee3c045d459de9040bf0ad78e1
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-11-14 17:07:08 +0000

    Remove Postgis tables from schema

--- db/schema.rb
+++ db/schema.rb
@@ -44,14 +44,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_13_135128) do
     t.index ["search_fields"], name: "index_application_version_on_search_fields", using: :gin
   end
 
-  create_table "spatial_ref_sys", primary_key: "srid", id: :integer, default: nil, force: :cascade do |t|
-    t.string "auth_name", limit: 256
-    t.integer "auth_srid"
-    t.string "srtext", limit: 2048
-    t.string "proj4text", limit: 2048
-    t.check_constraint "srid > 0 AND srid <= 998999", name: "spatial_ref_sys_srid_check"
-  end
-
   create_table "subscriber", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "subscriber_type", limit: 50, null: false
     t.text "webhook_url", null: false
@@ -70,64 +62,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_13_135128) do
 
   add_foreign_key "application_version", "application", name: "application_version_application_id_fkey"
 
-  create_view "geography_columns", sql_definition: <<-SQL
-      SELECT current_database() AS f_table_catalog,
-      n.nspname AS f_table_schema,
-      c.relname AS f_table_name,
-      a.attname AS f_geography_column,
-      postgis_typmod_dims(a.atttypmod) AS coord_dimension,
-      postgis_typmod_srid(a.atttypmod) AS srid,
-      postgis_typmod_type(a.atttypmod) AS type
-     FROM pg_class c,
-      pg_attribute a,
-      pg_type t,
-      pg_namespace n
-    WHERE ((t.typname = 'geography'::name) AND (a.attisdropped = false) AND (a.atttypid = t.oid) AND (a.attrelid = c.oid) AND (c.relnamespace = n.oid) AND (c.relkind = ANY (ARRAY['r'::"char", 'v'::"char", 'm'::"char", 'f'::"char", 'p'::"char"])) AND (NOT pg_is_other_temp_schema(c.relnamespace)) AND has_table_privilege(c.oid, 'SELECT'::text));
-  SQL
-  create_view "geometry_columns", sql_definition: <<-SQL
-      SELECT (current_database())::character varying(256) AS f_table_catalog,
-      n.nspname AS f_table_schema,
-      c.relname AS f_table_name,
-      a.attname AS f_geometry_column,
-      COALESCE(postgis_typmod_dims(a.atttypmod), sn.ndims, 2) AS coord_dimension,
-      COALESCE(NULLIF(postgis_typmod_srid(a.atttypmod), 0), sr.srid, 0) AS srid,
-      (replace(replace(COALESCE(NULLIF(upper(postgis_typmod_type(a.atttypmod)), 'GEOMETRY'::text), st.type, 'GEOMETRY'::text), 'ZM'::text, ''::text), 'Z'::text, ''::text))::character varying(30) AS type
-     FROM ((((((pg_class c
-       JOIN pg_attribute a ON (((a.attrelid = c.oid) AND (NOT a.attisdropped))))
-       JOIN pg_namespace n ON ((c.relnamespace = n.oid)))
-       JOIN pg_type t ON ((a.atttypid = t.oid)))
-       LEFT JOIN ( SELECT s.connamespace,
-              s.conrelid,
-              s.conkey,
-              replace(split_part(s.consrc, ''''::text, 2), ')'::text, ''::text) AS type
-             FROM ( SELECT pg_constraint.connamespace,
-                      pg_constraint.conrelid,
-                      pg_constraint.conkey,
-                      pg_get_constraintdef(pg_constraint.oid) AS consrc
-                     FROM pg_constraint) s
-            WHERE (s.consrc ~~* '%geometrytype(% = %'::text)) st ON (((st.connamespace = n.oid) AND (st.conrelid = c.oid) AND (a.attnum = ANY (st.conkey)))))
-       LEFT JOIN ( SELECT s.connamespace,
-              s.conrelid,
-              s.conkey,
-              (replace(split_part(s.consrc, ' = '::text, 2), ')'::text, ''::text))::integer AS ndims
-             FROM ( SELECT pg_constraint.connamespace,
-                      pg_constraint.conrelid,
-                      pg_constraint.conkey,
-                      pg_get_constraintdef(pg_constraint.oid) AS consrc
-                     FROM pg_constraint) s
-            WHERE (s.consrc ~~* '%ndims(% = %'::text)) sn ON (((sn.connamespace = n.oid) AND (sn.conrelid = c.oid) AND (a.attnum = ANY (sn.conkey)))))
-       LEFT JOIN ( SELECT s.connamespace,
-              s.conrelid,
-              s.conkey,
-              (replace(replace(split_part(s.consrc, ' = '::text, 2), ')'::text, ''::text), '('::text, ''::text))::integer AS srid
-             FROM ( SELECT pg_constraint.connamespace,
-                      pg_constraint.conrelid,
-                      pg_constraint.conkey,
-                      pg_get_constraintdef(pg_constraint.oid) AS consrc
-                     FROM pg_constraint) s
-            WHERE (s.consrc ~~* '%srid(% = %'::text)) sr ON (((sr.connamespace = n.oid) AND (sr.conrelid = c.oid) AND (a.attnum = ANY (sr.conkey)))))
-    WHERE ((c.relkind = ANY (ARRAY['r'::"char", 'v'::"char", 'm'::"char", 'f'::"char", 'p'::"char"])) AND (NOT (c.relname = 'raster_columns'::name)) AND (t.typname = 'geometry'::name) AND (NOT pg_is_other_temp_schema(c.relnamespace)) AND has_table_privilege(c.oid, 'SELECT'::text));
-  SQL
   create_view "events_raw", sql_definition: <<-SQL
       SELECT application.id,
       application.application_type,
