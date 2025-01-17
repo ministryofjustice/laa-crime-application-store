05e0bff9b9e71b699f56c4c72d5e6d43eb2e097a
robin skilling <robin.skilling@digital.justice.gov.uk>
2025-01-17 16:23:11 +0000

    wip delete claim docs

--- app/jobs/nsm/delete_reviewed_claim_docs.rb
+++ app/jobs/nsm/delete_reviewed_claim_docs.rb
@@ -1,19 +1,20 @@
 module Nsm
   class DeleteReviewedClaimDocs < ApplicationJob
-    after_perform do |job|
-      Rails.logger "GDPR delete supporting docs for reviewed NSM claim : #{job.args.first}"
-    end
+    class GDPRDeletionError < StandardError; end
 
     def perform(submission)
-      purge_docs(submission)
-    end
+      now = Time.zone.now
+      supporting_evidences = submission.latest_version.application["supporting_evidences"]
 
-    def purge_docs(submission)
-      submission.supporting_evidence.map do |file|
-        file_uploader.destroy(file.file_path)
+      supporting_evidences.each do |document|
+        if file_uploader.destroy(document["file_path"])
+          Rails.logger.info "Deleted #{document['id']} for submission #{submission.id}"
+        else
+          Rails.logger.error "Delete failed for document: #{document['id']} submission: #{submission.id}"
+          raise GDPRDeletionError, "Failed delete document: #{document['id']} submission: #{submission.id}"
+        end
       end
 
-      now = Time.zone.now
       submission.with_lock do
         submission.current_version += 1
         submission.last_updated_at = now
@@ -23,33 +24,30 @@ module Nsm
         latest_version = submission.latest_version
         submission.ordered_submission_versions.create!(
           json_schema_version: latest_version.json_schema_version,
-          application: latest_version.application.merge("updated_at" => now, "uploads_purged" => true),
+          application: latest_version.application.merge("updated_at" => now,
+                                                        "uploads_purged" => true),
           version: submission.current_version,
         )
       end
     end
+
+  private
 
     def build_delete_supporting_evidence_event(now, current_version)
       {
         submission_version: current_version,
         id: SecureRandom.uuid,
         created_at: now,
-        details: {comment: "GDPR delete supporting evidence"},
+        details: { comment: "Automated GDPR deletion of supporting evidence" },
         updated_at: now,
-        event_type: "gdpr_delete_supporting_evidence",
+        event_type: "gdpr_supporting_evidence",
         public: true,
         does_not_constitute_update: true,
       }
     end
 
-  private
-
     def file_uploader
       @file_uploader ||= FileUpload::FileUploader.new
-    end
-
-    def submission
-      @submission ||= Submission.find(job.arguments.first)
     end
   end
 end
--- spec/jobs/nsm/schedule_delete_reviewed_docs_spec.rb
+++ spec/jobs/nsm/schedule_delete_reviewed_docs_spec.rb
@@ -1,7 +1,48 @@
 require "rails_helper"
 
-RSpec.describe Nsm::ScheduleDeleteReviewedDocs do
+RSpec.describe Nsm::ScheduleDeleteReviewedDocs, type: :job do
+  include ActiveJob::TestHelper
+
+  subject(:job) { described_class.new.perform }
+
   let!(:claim) { create(:submission, application_type:, state:, last_updated_at:) }
+
+  describe "#perform" do
+    context "when cron adds job to queue" do
+      let(:application_type) { "crm7" }
+      let(:state) { "granted" }
+      let(:last_updated_at) { 6.months.ago }
+
+      it "adds a job to the queue" do
+        expect{ described_class.new.perform }.to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
+      end
+    end
+
+    context "when claims" do
+      let(:application_type) { "crm7" }
+      let(:state) { "granted" }
+      let(:last_updated_at) { 6.months.ago }
+
+      it "expect Nsm::DeleteReviewedClaimDocs to be queued" do
+        expect(Nsm::DeleteReviewedClaimDocs).to receive(:perform_later).with(claim)
+        described_class.new.perform
+      end
+
+      it "adds a job to the queue" do
+      expect{ described_class.new.perform }.to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
+      end
+    end
+
+    context "when no claims" do
+      let(:application_type) { "crm4" }
+      let(:state) { "granted" }
+      let(:last_updated_at) { 6.months.ago }
+
+      it "does not add a job to the queue" do
+       expect{ described_class.new.perform }.not_to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size)
+      end
+    end
+  end
 
   describe "#filterd_claims" do
     context "when PA" do
