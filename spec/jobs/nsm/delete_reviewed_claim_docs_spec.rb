c400b3b83fa396f6686099797389a9cefdd44bc7
robin skilling <robin.skilling@digital.justice.gov.uk>
2025-01-17 16:23:11 +0000

    wip delete claim docs

--- spec/jobs/nsm/delete_reviewed_claim_docs_spec.rb
+++ spec/jobs/nsm/delete_reviewed_claim_docs_spec.rb
@@ -1,21 +1,47 @@
 require "rails_helper"
 
-RSpec.describe DeleteReviewedClaimDocs do
+RSpec.describe Nsm::DeleteReviewedClaimDocs do
   describe "#perform" do
-    let(:claim) { create(:submission, application_type:, state:, last_updated_at:) }
+    let(:claim) { create(:submission) }
+    let(:file_uploader) { instance_double(FileUpload::FileUploader, destroy: true) }
+    let(:supporting_evidences) do
+      [
+        { id: 123, file_path: "123-a.pdf" },
+        { id: 456, file_path: "456-b.pdf" },
+        { id: 789, file_path: "789-c.pdf" },
+      ]
+    end
 
-    context "when PA" do
-      let(:application_type) { "crm4" }
+    before do
+      allow(FileUpload::FileUploader).to receive(:new).and_return(file_uploader)
+      claim.latest_version.update!(application: { supporting_evidences: })
+    end
 
-      context "when last_updated_at" do
-        describe "greater than 6 months ago" do
-          let(:state) { "granted" }
-          let(:last_updated_at) { 6.months.ago }
+    describe "#perform" do
+      it "calls destroy with filepath from supporting evidence" do
+        expect(file_uploader).to receive(:destroy).with("123-a.pdf").once
+        expect(file_uploader).to receive(:destroy).with("456-b.pdf").once
+        expect(file_uploader).to receive(:destroy).with("789-c.pdf").once
+        described_class.new.perform(claim)
+      end
 
-          it "is not included" do
-            expect(described_class.filterd_claims).not_to include(claim)
-          end
-        end
+      it "creates a new version" do
+        expect{ described_class.new.perform(claim) }.to change { claim.ordered_submission_versions.count }.by(1)
+      end
+
+      it "raise an error if file deletion fails" do
+        allow(file_uploader).to receive(:destroy).with("123-a.pdf").and_return(false)
+        expect { described_class.new.perform(claim) }.to raise_error(Nsm::DeleteReviewedClaimDocs::GDPRDeletionError)
+      end
+
+      it "adds an event to the claim" do
+        described_class.new.perform(claim)
+        expect(claim.reload.caseworker_history_events.last["event_type"]).to eq("gdpr_supporting_evidence")
+      end
+
+      it "adds a uploads_purged flag to the latest_version" do
+        described_class.new.perform(claim)
+        expect(claim.reload.latest_version.application["uploads_purged"]).to be(true)
       end
     end
   end
