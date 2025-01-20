c94ae056ab101fcc10efe5db9360a944c05235a6
robin skilling <robin.skilling@digital.justice.gov.uk>
2025-01-20 14:37:48 +0000

    swap objects for Ids

--- app/jobs/nsm/schedule_delete_reviewed_docs.rb
+++ app/jobs/nsm/schedule_delete_reviewed_docs.rb
@@ -6,7 +6,7 @@ module Nsm
       return if filtered_claims.empty?
 
       filtered_claims.each do |claim|
-        Nsm::DeleteReviewedClaimDocs.perform_later(claim)
+        Nsm::DeleteReviewedClaimDocs.perform_later(claim.id)
       end
     end
 
@@ -17,7 +17,7 @@ module Nsm
         latest_version.version = (SELECT MAX(version)
         FROM application_version all_versions
         WHERE all_versions.application_id = application.id)")
-        .where(state:, application_type: "crm7", last_updated_at: ..6.months.ago)
+        .where(state:, application_type: "crm7", updated_at: ..6.months.ago)
         .where("(latest_version.application->>'uploads_purged')::boolean IS false OR (latest_version.application->>'uploads_purged')::jsonb IS NULL")
     end
   end
