dc372eeea4b4b960420634031fc6f98df5582f88
robin skilling <robin.skilling@digital.justice.gov.uk>
2025-01-21 17:16:58 +0000

    linter

--- spec/services/file_upload/file_uploader_spec.rb
+++ spec/services/file_upload/file_uploader_spec.rb
@@ -1,10 +1,13 @@
 require "rails_helper"
 
 RSpec.describe FileUpload::FileUploader do
+  let(:s3_object) { instance_double(Aws::S3::Object, delete: nil) }
+
   describe "#destroy" do
     it "deletes the file" do
-      allow(S3_BUCKET).to receive(:object).with("aaaa-bbbb-1234").and_return(double(delete: true))
-      expect(described_class.new.destroy("aaaa-bbbb-1234")).to be_truthy
+      allow(S3_BUCKET).to receive(:object).with("aaaa-bbbb-1234").and_return(s3_object)
+      expect(s3_object).to receive(:delete)
+      described_class.new.destroy("aaaa-bbbb-1234")
     end
   end
 end
