4a0f7cbe9e92bd2c74ff002045f519a49d410eb2
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-01-22 18:15:55 +0000

    fix rubocop issues

--- spec/lib/tasks/crm457_2403_spec.rb
+++ spec/lib/tasks/crm457_2403_spec.rb
@@ -6,37 +6,37 @@ RSpec.describe "CRM457_2403:backfill_last_updated_at", type: :task do
   let(:pre_ssot_submission_id) { SecureRandom.uuid }
   let(:crm7_provider_updated_events) do
     [
-        {
-          "id" => "b294b8fb-5353-40ea-a073-76655c34b0e7",
-          "created_at" => "2024-11-10T18:08:24.454Z",
-          "event_type" => "new_version",
-          "submission_version" => 1,
-        },
-        {
-          "id" => "5e85599a-1eaa-4f2e-8401-845456484544",
-          "created_at" => "2024-11-11T18:08:24.454Z",
-          "event_type" => "send_back",
-          "submission_version" => 1,
-        },
-        {
-          "id" => "3d96c9e5-1e46-4e50-861a-0e7c46ed6a8e",
-          "created_at" => "2024-11-12T18:08:24.454Z",
-          "event_type" => "new_version",
-          "submission_version" => 3,
-        },
-        {
-          "id" => "26165504-7d4b-4eab-b1d6-a465bf110776",
-          "created_at" => "2024-11-13T18:08:24.454Z",
-          "event_type" => "send_back",
-          "submission_version" => 3,
-        },
-        {
-          "id" => "12768821-799b-4e63-af13-d200620ceeb9",
-          "created_at" => "2024-11-17T18:08:24.454Z",
-          "event_type" => "new_version",
-          "submission_version" => 5,
-        },
-      ]
+      {
+        "id" => "b294b8fb-5353-40ea-a073-76655c34b0e7",
+        "created_at" => "2024-11-10T18:08:24.454Z",
+        "event_type" => "new_version",
+        "submission_version" => 1,
+      },
+      {
+        "id" => "5e85599a-1eaa-4f2e-8401-845456484544",
+        "created_at" => "2024-11-11T18:08:24.454Z",
+        "event_type" => "send_back",
+        "submission_version" => 1,
+      },
+      {
+        "id" => "3d96c9e5-1e46-4e50-861a-0e7c46ed6a8e",
+        "created_at" => "2024-11-12T18:08:24.454Z",
+        "event_type" => "new_version",
+        "submission_version" => 3,
+      },
+      {
+        "id" => "26165504-7d4b-4eab-b1d6-a465bf110776",
+        "created_at" => "2024-11-13T18:08:24.454Z",
+        "event_type" => "send_back",
+        "submission_version" => 3,
+      },
+      {
+        "id" => "12768821-799b-4e63-af13-d200620ceeb9",
+        "created_at" => "2024-11-17T18:08:24.454Z",
+        "event_type" => "new_version",
+        "submission_version" => 5,
+      },
+    ]
   end
   let(:crm7_no_provider_updated_events) do
     [
@@ -73,7 +73,7 @@ RSpec.describe "CRM457_2403:backfill_last_updated_at", type: :task do
       state: "provider_updated",
       updated_at: pre_ssot_date,
       last_updated_at: pre_ssot_date,
-      events: crm7_provider_updated_events
+      events: crm7_provider_updated_events,
     )
   end
   let(:crm7_post_ssot_provider_updated_submission_id) { SecureRandom.uuid }
@@ -85,7 +85,7 @@ RSpec.describe "CRM457_2403:backfill_last_updated_at", type: :task do
       state: "provider_updated",
       updated_at: post_ssot_date,
       last_updated_at: pre_ssot_date,
-      events: crm7_provider_updated_events
+      events: crm7_provider_updated_events,
     )
   end
   let(:crm7_post_ssot_non_provider_updated_submission_id) { SecureRandom.uuid }
@@ -97,7 +97,7 @@ RSpec.describe "CRM457_2403:backfill_last_updated_at", type: :task do
       state: "sent_back",
       updated_at: post_ssot_date,
       last_updated_at: pre_ssot_date,
-      events: crm7_no_provider_updated_events
+      events: crm7_no_provider_updated_events,
     )
   end
   let(:crm4_provider_updated_events) do
@@ -171,7 +171,7 @@ RSpec.describe "CRM457_2403:backfill_last_updated_at", type: :task do
       state: "provider_updated",
       updated_at: post_ssot_date,
       last_updated_at: pre_ssot_date,
-      events: crm4_provider_updated_events
+      events: crm4_provider_updated_events,
     )
   end
   let(:crm4_post_ssot_non_provider_updated_submission_id) { SecureRandom.uuid }
@@ -183,7 +183,7 @@ RSpec.describe "CRM457_2403:backfill_last_updated_at", type: :task do
       state: "sent_back",
       updated_at: post_ssot_date,
       last_updated_at: pre_ssot_date,
-      events: crm4_no_provider_updated_events
+      events: crm4_no_provider_updated_events,
     )
   end
 
