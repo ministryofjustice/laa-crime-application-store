4eb3d9ddd4ba370e1725a097dee8085cacef089a
robin skilling <robin.skilling@digital.justice.gov.uk>
2025-01-23 14:29:56 +0000

    switch last_updated_at

--- app/jobs/nsm/schedule_delete_reviewed_docs.rb
+++ app/jobs/nsm/schedule_delete_reviewed_docs.rb
@@ -17,7 +17,7 @@ module Nsm
         latest_version.version = (SELECT MAX(version)
         FROM application_version all_versions
         WHERE all_versions.application_id = application.id)")
-        .where(state:, application_type: "crm7", updated_at: ..6.months.ago)
+        .where(state:, application_type: "crm7", last_updated_at: ..6.months.ago.end_of_day)
         .where("(latest_version.application->>'gdpr_documents_deleted')::boolean IS false OR (latest_version.application->>'gdpr_documents_deleted')::jsonb IS NULL")
     end
   end
