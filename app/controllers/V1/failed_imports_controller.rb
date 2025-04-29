b74c31379619a49ba2ce86b151f19c3980b97b14
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-04-29 15:37:14 +0100

    don't respond with created object

--- app/controllers/V1/failed_imports_controller.rb
+++ app/controllers/V1/failed_imports_controller.rb
@@ -3,8 +3,8 @@ module V1
     def create
       raise AlreadyExistsError if FailedImport.find_by(id: params[:id])
 
-      failed_import = FailedImport.create!(id: params[:id], provider_id: params[:provider_id])
-      render json: failed_import, status: :created
+      FailedImport.create!(id: params[:id], provider_id: params[:provider_id])
+      head :created
     rescue AlreadyExistsError
       head :conflict
     rescue ActiveRecord::RecordInvalid
