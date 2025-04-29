28d8730fbc5e2062b063feaa2cf71da8117d6ac0
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-04-29 14:40:15 +0100

    put creation code directly in controller

--- app/controllers/V1/failed_imports_controller.rb
+++ app/controllers/V1/failed_imports_controller.rb
@@ -1,7 +1,13 @@
 module V1
   class FailedImportsController < ApplicationController
     def create
-      failed_import = ::FailedImports::CreationService.call(params)
+      raise AlreadyExistsError if FailedImport.find_by(id: params[:id])
+
+      FailedImport.transaction do
+        failed_import = FailedImport.create!(id: params[:id], provider_id: params[:provider_id])
+        failed_import
+      end
+
       render json: failed_import, status: :created
     rescue ::FailedImports::CreationService::AlreadyExistsError
       head :conflict
