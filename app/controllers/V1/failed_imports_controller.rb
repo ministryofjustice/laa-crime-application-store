5097fb8578b8387443045d2f3f38024c9d00aecd
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-04-30 10:25:39 +0100

    fix rubocop issues

--- app/controllers/V1/failed_imports_controller.rb
+++ app/controllers/V1/failed_imports_controller.rb
@@ -1,5 +1,9 @@
 module V1
   class FailedImportsController < ApplicationController
+    def show
+      render json: current_import_error
+    end
+
     def create
       @current_import_error = FailedImport.create!(provider_id: params[:provider_id], details: params[:details])
       render json: current_import_error, status: :created
@@ -7,11 +11,7 @@ module V1
       head :unprocessable_entity
     end
 
-    def show
-      render json: current_import_error
-    end
-
-    private
+  private
 
     def current_import_error
       @current_import_error ||= Failed_Import.find(params[:id])
