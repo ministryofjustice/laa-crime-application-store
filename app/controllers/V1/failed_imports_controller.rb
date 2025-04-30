5c57a4f5d20d7abc0ee1ac89f8518333d251f95b
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-04-30 09:16:31 +0100

    simplify failed import creation by autogenerating id

--- app/controllers/V1/failed_imports_controller.rb
+++ app/controllers/V1/failed_imports_controller.rb
@@ -1,12 +1,8 @@
 module V1
   class FailedImportsController < ApplicationController
     def create
-      raise AlreadyExistsError if FailedImport.find_by(id: params[:id])
-
-      FailedImport.create!(id: params[:id], provider_id: params[:provider_id])
+      FailedImport.create!(provider_id: params[:provider_id])
       head :created
-    rescue AlreadyExistsError
-      head :conflict
     rescue ActiveRecord::RecordInvalid
       head :unprocessable_entity
     end
