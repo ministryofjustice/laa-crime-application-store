baab39f130943c043389bd6079373b2595181810
robin skilling <robin.skilling@digital.justice.gov.uk>
2025-05-28 15:20:52 +0100

    add error_type to failed_imports

--- spec/factories/failed_imports.rb
+++ spec/factories/failed_imports.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :failed_import do
     id { SecureRandom.uuid }
     provider_id { SecureRandom.uuid }
+    error_type { 'UNKNOWN' }
     details { "an error" }
   end
 end
