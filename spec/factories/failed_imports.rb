ef12ec4227ac4ec7bbc3cfe7b00ad3467b496c41
robin skilling <robin.skilling@digital.justice.gov.uk>
2025-06-02 12:41:55 +0100

    linter fixes

--- spec/factories/failed_imports.rb
+++ spec/factories/failed_imports.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :failed_import do
     id { SecureRandom.uuid }
     provider_id { SecureRandom.uuid }
-    error_type { 'UNKNOWN' }
+    error_type { "UNKNOWN" }
     details { "an error" }
   end
 end
