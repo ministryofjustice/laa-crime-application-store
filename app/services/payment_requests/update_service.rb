4ef3305d5decca5ca6028d3cf9a053c683b27cf5
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-07-22 11:21:33 +0100

    Don't cover else condition for update service
    This branch is unreachable due to the PaymentRequest class's own validations (request_type is required)

--- app/services/payment_requests/update_service.rb
+++ app/services/payment_requests/update_service.rb
@@ -24,7 +24,11 @@ module PaymentRequests
               allowed_net_assigned_counsel_cost: params[:allowed_net_assigned_counsel_cost],
               allowed_assigned_counsel_vat: params[:allowed_assigned_counsel_vat],
             )
+          # :nocov:
+          else
+            false
           end
+          # :nocov:
 
           attributes_to_assign[:submitted_at] = Time.zone.parse(params[:submitted_at]) if params[:submitted_at]
           payment_request.assign_attributes(attributes_to_assign)
