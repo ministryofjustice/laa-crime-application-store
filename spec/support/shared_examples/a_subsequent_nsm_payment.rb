22d34fc0484329428bb1d9ad1c8ab00d5f421d3a
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-07-30 16:38:54 +0100

    fix shared examples

--- spec/support/shared_examples/a_subsequent_nsm_payment.rb
+++ spec/support/shared_examples/a_subsequent_nsm_payment.rb
@@ -3,14 +3,14 @@ RSpec.shared_examples "a subsequent nsm payment" do |request_type|
 
   it "fails when no laa reference is supplied" do
     create(:payment_request, id: payment_id, request_type: request_type, submitted_at: nil)
-    patch "/v1/payment_requests/#{payment_id}/link"
+    patch "/v1/payment_requests/#{payment_id}/link_payable"
 
     expect(response).to have_http_status(:unprocessable_entity)
   end
 
   it "fails when laa reference is supplied but doesn't link to a record" do
     create(:payment_request, id: payment_id, request_type: request_type, submitted_at: nil)
-    patch "/v1/payment_requests/#{payment_id}/link", params: {
+    patch "/v1/payment_requests/#{payment_id}/link_payable", params: {
       laa_reference: "LAA-abc123",
     }
 
@@ -21,7 +21,7 @@ RSpec.shared_examples "a subsequent nsm payment" do |request_type|
     create(:payment_request, id: payment_id, request_type: request_type, submitted_at: nil)
     create(:assigned_counsel_claim, laa_reference: "LAA-abc123")
 
-    patch "/v1/payment_requests/#{payment_id}/link", params: {
+    patch "/v1/payment_requests/#{payment_id}/link_payable", params: {
       laa_reference: "LAA-abc123",
     }
 
@@ -31,7 +31,7 @@ RSpec.shared_examples "a subsequent nsm payment" do |request_type|
   it "is successful and links NsmClaim when a record with the laa reference exists" do
     create(:payment_request, id: payment_id, request_type: request_type, submitted_at: nil)
     create(:nsm_claim, laa_reference: "LAA-abc123")
-    patch "/v1/payment_requests/#{payment_id}/link", params: {
+    patch "/v1/payment_requests/#{payment_id}/link_payable", params: {
       laa_reference: "LAA-abc123",
     }
 
