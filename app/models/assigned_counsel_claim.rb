f40cf391db1c23cccf9411477eac708fc1b70b71
robin skilling <robin.skilling@digital.justice.gov.uk>
2025-07-31 14:59:06 +0100

    tighten regexp

--- app/models/assigned_counsel_claim.rb
+++ app/models/assigned_counsel_claim.rb
@@ -3,6 +3,6 @@ class AssignedCounselClaim < ApplicationRecord
   belongs_to :nsm_claim, optional: true
 
   validates :laa_reference, presence: true
-  validates :counsel_office_code, format: { with: /\A[0-9a-zA-Z]+\z/,
-    message: "only allows alphanumeric string" }
+  validates :counsel_office_code, format: { with: /\A\d[a-zA-Z0-9]*[a-zA-Z]\z/,
+    message: "only allows alphanumeric string starting with a number and ending in a letter" }
 end
