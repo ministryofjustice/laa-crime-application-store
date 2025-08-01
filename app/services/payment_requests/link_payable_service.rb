8ce47850718f75eb500d396b325581a64b12a6f4
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-08-01 17:05:31 +0100

    fix supplemental error handling

--- app/services/payment_requests/link_payable_service.rb
+++ app/services/payment_requests/link_payable_service.rb
@@ -8,7 +8,7 @@ module PaymentRequests
         if params[:laa_reference].present?
           claim = find_referred_claim(params[:laa_reference])
           submission = find_referred_submission(params[:laa_reference])
-          raise PaymentLinkError, I18n.t("errors.payment_request.legacy_supplemental") if submission && is_supplemental_claim?(claim)
+          raise PaymentLinkError, I18n.t("errors.payment_request.legacy_supplemental") if submission && is_supplemental?(submission)
         end
 
         case payment_request.request_type
@@ -79,10 +79,8 @@ module PaymentRequests
       end
 
       # rubocop:disable Style/SafeNavigationChainLength
-      def is_supplemental_claim?(claim)
-        return false unless claim.is_a?(NsmClaim)
-
-        claim.submission&.latest_version&.application&.dig("supplemental_claim") == "yes"
+      def is_supplemental?(submission)
+        submission.latest_version.application.dig("supplemental_claim") == "yes"
       end
       # rubocop:enable Style/SafeNavigationChainLength
     end
