3e95307ed63c24b037bd627770f24b5af251ccba
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-08-04 10:40:02 +0100

    add and apply the migration

---
+++ db/migrate/20250804091824_update_payment_fields.rb
@@ -0,0 +1,16 @@
+class UpdatePaymentFields < ActiveRecord::Migration[8.0]
+  def change
+    rename_column :nsm_claims, :client_surname, :client_last_name
+    rename_column :nsm_claims, :case_concluded_date, :work_completed_date
+    add_column :nsm_claims, :outcome_code, :string
+    add_column :nsm_claims, :matter_type, :string
+    add_column :nsm_claims, :youth_court, :boolean
+
+    add_column :assigned_counsel_claims, :date_received, :datetime
+    add_column :assigned_counsel_claims, :ufn, :string
+    add_column :assigned_counsel_claims, :solicitor_office_code, :string
+    add_column :assigned_counsel_claims, :client_last_name, :string
+
+    add_column :payment_requests, :date_claim_received, :datetime
+  end
+end
