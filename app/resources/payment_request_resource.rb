b5776cfd6f7d3a180deb06e6b27d0a9112b56c17
robin skilling <robin.skilling@digital.justice.gov.uk>
2025-08-05 16:34:37 +0100

    remove root key

--- app/resources/payment_request_resource.rb
+++ app/resources/payment_request_resource.rb
@@ -1,21 +1,20 @@
 class PaymentRequestResource
   include Alba::Resource
 
-  root_key :payment_request
-
   attributes :submitter_id, :request_type,
   :profit_cost, :allowed_profit_cost,
   :travel_cost, :allowed_travel_cost,
   :waiting_cost, :allowed_waiting_cost,
   :disbursement_cost, :allowed_disbursement_cost,
-  :submitted_at, :date_claim_received,
-  :net_assigned_counsel_cost, :assigned_counsel_vat,
-  :allowed_assigned_counsel_vat,
-  :created_at, :updated_at,
+  :submitted_at, :date_claim_received
 
-  attribute :allowed_assigned_counsel_cost do |resource|
-    resource.allowed_net_assigned_counsel_cost
-  end
+  attributes :net_assigned_counsel_cost,
+    :assigned_counsel_vat,
+    :allowed_net_assigned_counsel_cost,
+    :allowed_assigned_counsel_vat,
+    if: proc { |payment_request, attrs| payment_request.payable.is_a? AssignedCounselClaim }
+
+  attributes :created_at, :updated_at
 
   one :payable, resource: ->(resource) do
     "#{resource.class.name}Resource".constantize
