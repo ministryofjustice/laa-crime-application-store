4d05d19dcdc9e505beee800b4252ff959eb0f6ce
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-08-18 16:32:53 +0100

    fix rubocop errors

--- spec/lib/host_env_spec.rb
+++ spec/lib/host_env_spec.rb
@@ -1,57 +1,57 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe HostEnv do
-  context 'when local development rails environment' do
+  context "when local development rails environment" do
     before do
       allow(Rails.env).to receive(:development?).and_return(true)
     end
 
-    describe 'HostEnv.uat?' do
-      it 'returns false' do
+    describe "HostEnv.uat?" do
+      it "returns false" do
         expect(described_class.uat?).to be false
       end
     end
 
-    describe 'HostEnv.production?' do
-      it 'returns false' do
+    describe "HostEnv.production?" do
+      it "returns false" do
         expect(described_class.production?).to be false
       end
     end
 
-    describe '.local?' do
-      it 'returns true' do
+    describe ".local?" do
+      it "returns true" do
         expect(described_class.local?).to be true
       end
     end
   end
 
-  context 'when local test rails environment' do
-    describe 'HostEnv.uat?' do
-      it 'returns false' do
+  context "when local test rails environment" do
+    describe "HostEnv.uat?" do
+      it "returns false" do
         expect(described_class.uat?).to be false
       end
     end
 
-    describe '.production?' do
-      it 'returns true' do
+    describe ".production?" do
+      it "returns true" do
         expect(described_class.production?).to be false
       end
     end
 
-    describe '.local?' do
-      it 'returns true' do
+    describe ".local?" do
+      it "returns true" do
         expect(described_class.local?).to be true
       end
     end
   end
 
-  describe 'ENV variable is set in production envs' do
+  describe "ENV variable is set in production envs" do
     before do
-      allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('production'))
-      allow(ENV).to receive(:fetch).with('ENV').and_return(env_name)
+      allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("production"))
+      allow(ENV).to receive(:fetch).with("ENV").and_return(env_name)
     end
 
-    context 'when uat host' do
+    context "when uat host" do
       let(:env_name) { HostEnv::UAT }
 
       it { expect(described_class.local?).to be(false) }
@@ -59,7 +59,7 @@ describe HostEnv do
       it { expect(described_class.production?).to be(false) }
     end
 
-    context 'when production host' do
+    context "when production host" do
       let(:env_name) { HostEnv::PRODUCTION }
 
       it { expect(described_class.local?).to be(false) }
@@ -67,8 +67,8 @@ describe HostEnv do
       it { expect(described_class.production?).to be(true) }
     end
 
-    context 'when unknown host' do
-      let(:env_name) { 'foobar' }
+    context "when unknown host" do
+      let(:env_name) { "foobar" }
 
       it { expect(described_class.local?).to be(false) }
       it { expect(described_class.uat?).to be(false) }
@@ -76,15 +76,15 @@ describe HostEnv do
     end
   end
 
-  describe 'when is a production env and the ENV variable is not set' do
+  describe "when is a production env and the ENV variable is not set" do
     before do
-      allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('production'))
+      allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("production"))
     end
 
-    it 'raises an exception so we are fully aware' do
-      expect do
+    it "raises an exception so we are fully aware" do
+      expect {
         described_class.production?
-      end.to raise_exception(KeyError)
+      }.to raise_exception(KeyError)
     end
   end
 end
