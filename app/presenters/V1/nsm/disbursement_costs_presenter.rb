2f94fe918a67fb384f0e669ebca62f65bbe9885f
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-09-17 10:20:29 +0100

    convert claimed_cost to BigDecimal

--- app/presenters/V1/nsm/disbursement_costs_presenter.rb
+++ app/presenters/V1/nsm/disbursement_costs_presenter.rb
@@ -9,7 +9,7 @@ module V1
       def data_for_calculation
         {
           disbursement_type: @disbursement["disbursement_type"],
-          claimed_cost: @disbursement["total_cost_without_vat"],
+          claimed_cost: BigDecimal(@disbursement["total_cost_without_vat"]),
           claimed_miles: BigDecimal(disbursement_miles),
           claimed_apply_vat: apply_vat?,
           assessed_cost: 0,
