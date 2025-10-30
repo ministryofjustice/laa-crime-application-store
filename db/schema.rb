88025d7d02a0c496b2b706e0c330208db12d0905
robin skilling <robin.skilling@digital.justice.gov.uk>
2025-10-30 16:49:50 +0000

    update and sync cost names

--- db/schema.rb
+++ db/schema.rb
@@ -89,15 +89,15 @@ ActiveRecord::Schema[8.1].define(version: 2025_10_27_144609) do
     t.decimal "allowed_total", precision: 10, scale: 2
     t.decimal "allowed_travel_cost", precision: 10, scale: 2
     t.decimal "allowed_waiting_cost", precision: 10, scale: 2
-    t.decimal "assigned_counsel_vat", precision: 10, scale: 2
+    t.decimal "claimed_assigned_counsel_vat", precision: 10, scale: 2
     t.decimal "claimed_disbursement_cost", precision: 10, scale: 2
+    t.decimal "claimed_net_assigned_counsel_cost", precision: 10, scale: 2
     t.decimal "claimed_profit_cost", precision: 10, scale: 2
     t.decimal "claimed_total", precision: 10, scale: 2
     t.decimal "claimed_travel_cost", precision: 10, scale: 2
     t.decimal "claimed_waiting_cost", precision: 10, scale: 2
     t.datetime "created_at", null: false
     t.datetime "date_received"
-    t.decimal "net_assigned_counsel_cost", precision: 10, scale: 2
     t.uuid "payment_request_claim_id"
     t.string "request_type"
     t.datetime "submitted_at"
