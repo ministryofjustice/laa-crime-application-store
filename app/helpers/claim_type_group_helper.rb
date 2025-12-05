ae0e8c0e5159c75339eeec85889de30545785bff
robin skilling <robin.skilling@digital.justice.gov.uk>
2025-12-05 14:11:42 +0000

    scope search to a claim type if required

---
+++ app/helpers/claim_type_group_helper.rb
@@ -0,0 +1,20 @@
+module ClaimTypeGroupHelper
+  CLAIM_TYPE_MAP = {
+    "NsmClaim" => %w[
+    breach_of_injunction
+    non_standard_magistrate
+    non_standard_mag_supplemental
+    non_standard_mag_appeal
+    non_standard_mag_amendment
+  ],
+    "AssignedCounselClaim" => %w[
+    assigned_counsel
+    assigned_counsel_appeal
+    assigned_counsel_amendment
+  ],
+  }.freeze
+
+  def find_claim_type_group(request_type)
+    CLAIM_TYPE_MAP.find { |_, types| types.include?(request_type) }&.first
+  end
+end
