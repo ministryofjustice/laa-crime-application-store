bbc4147208d253d8dcd1492e2b0372bee140b3ba
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-12-11 14:08:32 +0000

    use crime forms common for office code validator

--- app/validators/office_code_validator.rb
+++
@@ -1,9 +0,0 @@
-class OfficeCodeValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    expected_format = /\A\d[a-zA-Z0-9]*[a-zA-Z]\z/
-
-    unless expected_format.match?(value) || value.nil?
-      record.errors.add attribute, I18n.t("errors.office_code")
-    end
-  end
-end
