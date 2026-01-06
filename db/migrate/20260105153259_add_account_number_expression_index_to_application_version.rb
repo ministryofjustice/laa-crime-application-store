a149541e45ac13ef7089b00419affdb35daac47b
robin skilling <robin.skilling@digital.justice.gov.uk>
2026-01-06 11:02:16 +0000

    index application account_number

---
+++ db/migrate/20260105153259_add_account_number_expression_index_to_application_version.rb
@@ -0,0 +1,31 @@
+class AddAccountNumberExpressionIndexToApplicationVersion < ActiveRecord::Migration[8.1]
+  disable_ddl_transaction! #allows concurrently
+
+  INDEX_NAME = "idx_application_version_on_account_number"
+
+  def up
+    return if index_exists_by_name?
+
+    execute <<~SQL
+      CREATE INDEX CONCURRENTLY #{INDEX_NAME}
+      ON application_version (
+        (application -> 'firm_office' ->> 'account_number')
+      );
+    SQL
+  end
+
+  def down
+    return unless index_exists_by_name?
+
+    execute <<~SQL
+      DROP INDEX CONCURRENTLY #{INDEX_NAME};
+    SQL
+  end
+
+  private
+
+  def index_exists_by_name?
+    ActiveRecord::Base.connection.indexes(:application_version)
+      .any? { |idx| idx.name == INDEX_NAME }
+  end
+end
