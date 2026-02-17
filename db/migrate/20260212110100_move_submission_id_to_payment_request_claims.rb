fdf84c1505755a37ef43038a454fe22be3c63dbf
robin skilling <robin.skilling@digital.justice.gov.uk>
2026-02-17 12:41:28 +0000

    optional data fix

--- db/migrate/20260212110100_move_submission_id_to_payment_request_claims.rb
+++ db/migrate/20260212110100_move_submission_id_to_payment_request_claims.rb
@@ -2,14 +2,14 @@ class MoveSubmissionIdToPaymentRequestClaims < ActiveRecord::Migration[8.1]
   def up
     add_column :payment_request_claims, :submission_id, :uuid
 
-    execute <<~SQL
-      UPDATE payment_request_claims prc
-      SET submission_id = pr.submission_id
-      FROM payment_requests pr
-      WHERE pr.payment_request_claim_id = prc.id
-        AND pr.submission_id IS NOT NULL
-        AND prc.submission_id IS NULL;
-    SQL
+    # execute <<~SQL
+    #   UPDATE payment_request_claims prc
+    #   SET submission_id = pr.submission_id
+    #   FROM payment_requests pr
+    #   WHERE pr.payment_request_claim_id = prc.id
+    #     AND pr.submission_id IS NOT NULL
+    #     AND prc.submission_id IS NULL;
+    # SQL
 
     remove_column :payment_requests, :submission_id
   end
@@ -17,13 +17,13 @@ class MoveSubmissionIdToPaymentRequestClaims < ActiveRecord::Migration[8.1]
   def down
     add_column :payment_requests, :submission_id, :uuid
 
-    execute <<~SQL
-      UPDATE payment_requests pr
-      SET submission_id = prc.submission_id
-      FROM payment_request_claims prc
-      WHERE pr.payment_request_claim_id = prc.id
-        AND prc.submission_id IS NOT NULL;
-    SQL
+    # execute <<~SQL
+    #   UPDATE payment_requests pr
+    #   SET submission_id = prc.submission_id
+    #   FROM payment_request_claims prc
+    #   WHERE pr.payment_request_claim_id = prc.id
+    #     AND prc.submission_id IS NOT NULL;
+    # SQL
 
     remove_column :payment_request_claims, :submission_id
   end
