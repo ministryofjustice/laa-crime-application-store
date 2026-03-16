df055960964ebb927fd86bed82f1403740291bb2
robin skilling <robin.skilling@digital.justice.gov.uk>
2026-03-16 11:57:23 +0000

    update resources

--- app/resources/payment_request_resource.rb
+++ app/resources/payment_request_resource.rb
@@ -7,25 +7,25 @@ class PaymentRequestResource
              :claimed_travel_cost, :allowed_travel_cost,
              :claimed_waiting_cost, :allowed_waiting_cost,
              :claimed_disbursement_cost, :allowed_disbursement_cost,
-             if: proc { |payment_request, _attrs| payment_request.payment_request_claim.is_a? NsmClaim }
+             if: proc { |payment_request, _attrs| payment_request.payable_claim.is_a? NsmClaim }
   attributes :claimed_net_assigned_counsel_cost,
              :claimed_assigned_counsel_vat,
              :allowed_net_assigned_counsel_cost,
              :allowed_assigned_counsel_vat,
-             if: proc { |payment_request, _attrs| payment_request.payment_request_claim.is_a? AssignedCounselClaim }
+             if: proc { |payment_request, _attrs| payment_request.payable_claim.is_a? AssignedCounselClaim }
 
   attributes :claimed_total, :allowed_total,
              if: proc { |pr, _|
-               [NsmClaim, AssignedCounselClaim].any? { pr.payment_request_claim.is_a?(_1) }
+               [NsmClaim, AssignedCounselClaim].any? { pr.payable_claim.is_a?(_1) }
              }
 
   attributes :created_at, :updated_at
 
-  one :payment_request_claim, resource: lambda { |resource|
+  one :payable_claim, resource: lambda { |resource|
     "#{resource.class.name}Resource".constantize
   }, if: proc { params[:include_claim] }
 
   def submission_id(payment_request)
-    payment_request.payment_request_claim&.submission_id
+    payment_request.payable_claim&.submission_id
   end
 end
--- app/resources/payment_request_search_results_resource.rb
+++ app/resources/payment_request_search_results_resource.rb
@@ -5,9 +5,9 @@ class PaymentRequestSearchResultsResource
              :submitted_at, :submission_id
   attributes :created_at, :updated_at
 
-  one :payment_request_claim, resource: ClaimPaymentSearchResultsResource
+  one :payable_claim, resource: ClaimPaymentSearchResultsResource
 
   def submission_id(payment_request)
-    payment_request.payment_request_claim&.submission_id
+    payment_request.payable_claim&.submission_id
   end
 end
