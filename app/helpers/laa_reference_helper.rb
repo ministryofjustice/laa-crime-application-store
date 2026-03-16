a660efd45fb7602927f51bd07d402a7f2185f690
robin skilling <robin.skilling@digital.justice.gov.uk>
2026-03-16 11:59:29 +0000

    update laa_ref_helper

--- app/helpers/laa_reference_helper.rb
+++ app/helpers/laa_reference_helper.rb
@@ -1,7 +1,7 @@
 module LaaReferenceHelper
   def generate_laa_reference
     ActiveRecord::Base.transaction do
-      PaymentRequestClaim.lock
+      PayableClaim.lock
       loop do
         random_reference = "LAA-#{SecureRandom.alphanumeric(6)}"
         break random_reference unless reference_already_exists?(random_reference)
@@ -16,7 +16,7 @@ module LaaReferenceHelper
   end
 
   def find_referred_claim(laa_reference)
-    PaymentRequestClaim.find_by(laa_reference:)
+    PayableClaim.find_by(laa_reference:)
   end
 
   def find_referred_submission(laa_reference)
