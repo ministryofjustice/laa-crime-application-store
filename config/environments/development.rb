c5051d542781da82072b09f66995539c1d6f1060
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2026-04-01 14:22:22 +0100

    Remove active storage from service

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,6 @@ Rails.application.configure do
 
   config.active_job.queue_adapter = :sidekiq
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
-
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
