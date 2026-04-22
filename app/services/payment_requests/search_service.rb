0b2615528316c20258883048b6aa2340ec209e17
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2026-04-22 13:48:11 +0100

    allow search via solicitor firm name for payments

--- app/services/payment_requests/search_service.rb
+++ app/services/payment_requests/search_service.rb
@@ -22,7 +22,10 @@ module PaymentRequests
       claims = claims.where("LOWER(payable_claims.laa_reference) = ?", query_params[:laa_reference].downcase) if query_params[:laa_reference].present?
       claims = claims.where(payable_claims: { ufn: query_params[:ufn] }) if query_params[:ufn].present?
       claims = claims.where("LOWER(payable_claims.solicitor_office_code) = ?", query_params[:office_code].downcase) if query_params[:office_code].present?
-      claims = claims.where("LOWER(payable_claims.client_last_name) % ?::text", "%#{query_params[:client_last_name].downcase}%") if query_params[:client_last_name].present?
+      claims = claims.where(
+        "LOWER(payable_claims.solicitor_firm_name) % ?::text OR LOWER(payable_claims.client_last_name) % ?::text",
+        query_params[:free_text].downcase, query_params[:free_text].downcase
+      ) if query_params[:free_text].present?
       claims = claims.where(payable_claims: { submission_id: }) if submission_id
       claims.order(sort_clause)
     end
@@ -82,7 +85,7 @@ module PaymentRequests
         elsif /^\d.*[a-zA-Z]$/.match?(word)
           acc[:office_code] = word
         else
-          acc[:client_last_name] = word
+          acc[:free_text] = word
         end
       end
       @query_params ||= results
